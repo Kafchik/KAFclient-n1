320d8ca9da5bc2f4cd09e268c9f41d236cebf603
Kafchik <darmu2919@gmail.com>
2025-11-04 17:06:45 +0300

    Create loadkaf.lua

---
+++ loadkaf.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kafchik/KAFclient-n1/refs/heads/main/kafclient.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kafchik/KAFclient-n1/refs/heads/main/kafclient2.lua"))()
