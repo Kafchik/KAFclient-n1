cdff0c19d6dd68e969ed72bac0b4556ab069ad70
Kafchik <darmu2919@gmail.com>
2025-11-04 20:57:44 +0300

    loadkaf.lua

--- loadkaf.lua
+++ loadkaf.lua
@@ -1,2 +1,16 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Kafchik/KAFclient-n1/refs/heads/main/kafclient.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Kafchik/KAFclient-n1/refs/heads/main/kafclient2.lua"))()
+
+local success1, result1 = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kafchik/KAFclient-n1/refs/heads/main/kafclient.lua"))()
+end)
+
+local success2, result2 = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kafchik/KAFclient-n1/refs/heads/main/kafclient2.lua"))()
+end)
+
+if not success1 then
+    warn("Ошибка в первом скрипте: " .. result1)
+end
+
+if not success2 then
+    warn("Ошибка во втором скрипте: " .. result2)
+end
